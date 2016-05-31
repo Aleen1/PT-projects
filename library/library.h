561f3723261b62a18731794d83734c65ce6c5843
Aleen1 <alin.calina@yahoo.com>
2016-05-31 19:11:45 +0300

    final.

--- library/library.h
+++ library/library.h
@@ -5,8 +5,18 @@
 
     typedef struct Nod{
         int data;
-        struct Nod * next;
+        struct Nod *next;
     }Nod;
+
+    typedef struct Node{
+		int data;
+		struct Node *l, *r;
+	}Node;
+
+    typedef struct Node_L{
+		int data;
+		struct Node_L *next;
+	}Node_L;
 
     /**Push functions for single-linked lists**/
     void print_list(Nod *head);
@@ -25,6 +35,9 @@
     void readMat(int a[100][100], int n);
 
     /**Heap sort funtions**/
+    void initialise_heap(Node **root, int data);
+	void add_to_heap(Node *root, int data);
+	void heap_sort(Node *root, int *vec, int *sizee);
     void heapify(int arr[], int n, int i);
     void heapSort(int arr[], int n);
 
@@ -42,9 +55,25 @@
     void countSort(int arr[], int n, int exp);
     void radixsort(int arr[], int n);
 
+    /**List functions**/
+	void initialise_list(Node_L **node);
+	void add_to_list(Node_L **node, int data);
+	void remove_first_list(Node_L **node);
+	int get_first_elem_list(Node_L *node);
+
+    /**Queue functions**/
+    void initialise_queue(int *sq, int *eq);
+	void add_to_queue(int *queue, int *sq, int *eq, int MAX, int data);
+	int is_empty_queue(int *sq, int *eq);
+	void pop_queue(int *sq, int *eq, int MAX);
+	int  queue_front(int *queue, int *sq);
+
     /**Traversing graphs/trees functions**/
+    void DFS(Node_L *V[], int *viz, int t);
+	void BFS(Node_L *V[], int size, int *viz, int s);
     void dfs_mat(int root, int n);
     void bfs_mat(int root, int n);
+	void sort_top(Node_L *V[], int *viz, Node_L **stk, int t);
 
     /**Backtracking standard functions**/
     void inline init(int k);
@@ -53,9 +82,14 @@
     void printSolution(int k);
     int valid(int k);
     void bkt(int k);
+    void printSol_queen(int board[N][N]);
+    int isSafe(int board[N][N], int row, int col);
+    int solveNQUtil(int board[N][N], int col);
+    int solveNQ();
 
     /**Dynamic Programming algorithms**/
     int count_coin( int S[], int m, int n );    // need to read m(nr of coins) and n(the sum)
     int knapsack(int n, int g);                 // need to read n(nr of obj) and g(max value)
+    int MatrixChainOrder(int p[], int n);
 
 #endif
